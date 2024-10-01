9f85542ced51897bee6d3c2d4a77d819a4f27037
DARSHANR007 <darshanravichandran07@gmail.com>
2024-10-01 19:34:31 +0530

    Added Receipt mechanism

--- main.go
+++ main.go
@@ -1,44 +1,63 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/stripe/stripe-go/v79"
-	"log"
-	"os"
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/charge"
 )
 
-type Charge struct {
-	Amount        int64  `json:"amount"`
-	RecipientMail string `json:"recipient_mail"`
+// ChargeJSON incoming data for Stripe API
+type ChargeJSON struct {
+	Amount       int64  `json:"amount"`
+	ReceiptEmail string `json:"receiptEmail"`
 }
 
 func main() {
-
-	err := godotenv.Load()
-
+	// load .env file
+	err := godotenv.Load("api.env")
 	if err != nil {
-		log.Fatal("error loading: ", err)
+		log.Fatal("Error loading .env file")
 	}
 
+	// set up server
 	r := gin.Default()
 
-	r.GET("/", func(context *gin.Context) {
-		context.JSON(200, gin.H{"message": "This is the customers page"})
-
+	// basic hello world GET route
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
 	})
 
-	r.POST("/api/charges", func(context *gin.Context) {
+	// our basic charge API route
+	r.POST("/api/charges", func(c *gin.Context) {
+		// we will bind our JSON body to the `json` var
+		var json ChargeJSON
+		c.BindJSON(&json)
 
-		var json Charge
-		err := context.BindJSON(json)
-		if err != nil {
-			return
-		}
-
+		// Set Stripe API key
 		apiKey := os.Getenv("sk_test")
 		stripe.Key = apiKey
 
+		_, err := charge.New(&stripe.ChargeParams{
+			Amount:       stripe.Int64(json.Amount),
+			Currency:     stripe.String(string(stripe.CurrencyINR)),
+			Source:       &stripe.SourceParams{Token: stripe.String("tok_visa")},
+			ReceiptEmail: stripe.String(json.ReceiptEmail)})
+
+		if err != nil {
+			// Handle any errors from attempt to charge
+			c.String(http.StatusBadRequest, "Request failed")
+			return
+		}
+
+		c.String(http.StatusCreated, "Successfully charged")
 	})
 
+	r.Run(":8080")
 }
